04fafd81740fc8154cd84445fe7acda8501866e7
Murilo Messias <murilo.messias@gmail.com>
2021-08-17 15:53:46 -0300

    Resolução Exercícios

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -8,36 +8,12 @@ app.innerHTML = `
   <p>Boas vindas à semana de pré-work para o Boot camp em React.js 😁</p>
 `;
 
-const visibility = {
-  // Adicionar ao elemento o atributo display="block"
-  show: (element) => {
-    element.style.display = "block";
-    element.style.visibility = "visible";
-  },
-  // Adicionar ao elemento o atributo display="none"
-  hide: (element) => {
-    element.style.display = "none";
-    element.style.visibility = "hidden";
-  },
-};
-
-// Verificar se o elemento possui display="block" ou display="none"
-// Se estiver com display="block" e visibility="visible", executar hide(element).
-// Se estiver com display="none" e visibility="invisible", executar show(element)
-const toggle = (element) => {
-  const isVisible =
-    window.getComputedStyle(element).display === "block" &&
-    window.getComputedStyle(element).visibility === "visible";
-  isVisible ? visibility["hide"](element) : visibility["show"](element);
-};
-
-// Executar a função toggle() quando o usuário clicar no link,
-// prevenindo a ação padrão que um clique realiza.
+// Adicionar a classe "hide" ao realizar o clique no link
 link.addEventListener(
   "click",
-  (event) => {
-    event.preventDefault();
-    toggle(app);
+  (e) => {
+    e.preventDefault();
+    app.classList.toggle("hide");
   },
   false
 );

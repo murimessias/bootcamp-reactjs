b1f71e3fff4c5b2c2c98461a00fbb5b18ec1058f
Murilo Messias <murilo.messias@gmail.com>
2021-08-19 21:54:15 -0300

    Dia 03: Exercicio 02

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -21,3 +21,49 @@ input.addEventListener("input", (e) => {
 function fixCase(word) {
   return `${word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()}`;
 }
+
+// Criação do formulário e o campo de select.
+const form = document.querySelector('[data-js="form"]');
+const select = document.createElement("select");
+const colors = ["#ef2", "#3f3f", "#02f", "#d0f", "#0fd", "#a3a"];
+const colorsContainer = document.createElement("div");
+colorsContainer.style.display = "flex";
+
+// Criar a option a partir do Array "colors" iterando
+// sobre cada cor existente e colocando "value" e "textContent"
+colors.forEach((color) => {
+  const option = createOption(color);
+  select.appendChild(option);
+});
+
+// Executar a função de ouvir quando uma opção for selecionada
+// e criar uma div com a cor selecionada
+select.addEventListener("change", (e) => {
+  colorsContainer.innerHTML = "";
+
+  Array.from(e.target.selectedOptions).forEach((option) => {
+    const div = createDivColor(option.value);
+    colorsContainer.appendChild(div);
+  });
+});
+
+// Função para criar a div que vai receber o valor selecionado
+function createDivColor(value) {
+  const div = document.createElement("div");
+  div.style.width = "100px";
+  div.style.height = "100px";
+  div.style.background = value;
+  return div;
+}
+
+// Função para criar o option dentro do select
+function createOption(color) {
+  const option = document.createElement("option");
+  option.value = color;
+  option.textContent = color;
+  return option;
+}
+
+select.setAttribute("multiple", "");
+form.appendChild(select);
+document.body.appendChild(colorsContainer);
